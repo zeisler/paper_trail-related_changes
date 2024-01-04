70e0008661e25eacaef4949565c791e588cf2019
Jason Becker <jason@jbecker.co>
2024-01-03 19:59:03 -0500

    Splat

--- lib/paper_trail/related_changes/change.rb
+++ lib/paper_trail/related_changes/change.rb
@@ -17,11 +17,11 @@ module PaperTrail::RelatedChanges
 
     def to_h(*)
       self.diffs = diffs
-                     .group_by(&:source)
-                     .map { |k, g| [k, g.sort_by(&:source_rank)] } # ie. segments can be in display order
-                     .sort_by { |_, g| g[0].rank } # Direct attributes shown first
-                     .flat_map(&:last)
-                     .uniq
+                   .group_by(&:source)
+                   .map { |k, g| [k, g.sort_by(&:source_rank)] } # ie. segments can be in display order
+                   .sort_by { |_, g| g[0].rank } # Direct attributes shown first
+                   .flat_map(&:last)
+                   .uniq
       results    = super().except(:merge_into_root).map { |k, v| [k, v.is_a?(Array) ? v.map(&:to_h) : v] }.to_h
       results.delete(:children) if results[:children].nil?
       results
@@ -34,7 +34,7 @@ module PaperTrail::RelatedChanges
     end
 
     def add_diff(args)
-      self.diffs << PaperTrail::RelatedChanges::Serializer::Diff.new(args)
+      diffs << PaperTrail::RelatedChanges::Serializer::Diff.new(**args)
     end
   end
 end
